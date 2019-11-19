ad12cf543777e92d79cf500ce3ff645f4d05ad61
Alkesst <alec.augal@gmail.com>
2019-11-19 17:10:49 +0100

    Types done

--- src/@types/Biceater.d.ts
+++ src/@types/Biceater.d.ts
@@ -5,7 +5,37 @@ export interface JSONResult<T> {
     [index: string]: T;
 }
 
+export type Genre = 'Male' | 'Female';
+
 export interface User {
-    name: string
+    userId: number;
+    username: string;
     email: string;
+    name: string;
+    surname: string;
+    dateOfBirth: Date;
+    imagePath: string;
+    bio: string;
+    genre: Genre;
+    hobbies: string;
+}
+
+export interface BikeHireDockingStation {
+    // rellenar con datos de la api publica
+}
+
+export interface Comment {
+    commentId: number;
+    text: string;
+    date: Date;
+    authorId: User;
+    answersTo: Comment;
+    bikeHireDockingStation: BikeHireDockingStation;
+}
+
+export interface Rating {
+    ratingId: number;
+    bikeHireDockingStation: BikeHireDockingStation;
+    rating: 1 | 2 | 3 | 4 | 5;
+    author: User;
 }
