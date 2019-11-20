26eeb72d8fad5ff2f11e34c646dda721d6e6ab73
JesusPa98 <jesusparejo98@gmail.com>
2019-11-20 19:07:44 +0100

    Mapa integrado

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import {render} from 'react-dom'
 import {Map, Marker, Popup, TileLayer} from "react-leaflet";
+import "../styles/index.css"
 
 export const MapComponent : React.FC = () => {
-    const position: [number, number] = [51.505, -0.99];
+    const position: [number, number] = [36.71580426431715, -4.455611828876954];
     return (
       <>
           I'm trying the routing components from react.
-          <Map center={position} zoom={13}>
+          <Map center={position} zoom={13} id="mapid">
               <TileLayer
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                   attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
               />
               <Marker position={position}>
-                  <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+                  <Popup>Comprar Papatas fritas</Popup>
               </Marker>
           </Map>
       </>
