f048671b3890f5cf9dcc7391aa31d5077731400f
Jesús Parejo Aliaga <jesusparejo98@gmail.com>
2019-11-23 11:51:48 +0100

    Añadidas las rutas en el mapa (#9)
    * Types done
    
    * A mamarla
    
    * Navbar added.
    
    * Map added
    
    * Mapa integrado
    
    * Simpler navbar.
    
    * Subido con el error para que @alkesst pueda mirarlo
    
    * Added Routes
    
    * added coordinates to props
    
    * added routing style
    
    * Added navbar to map component.
    
    * Added node-sass.

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Map, Marker, Popup, TileLayer} from "react-leaflet";
+import Routing from "./RoutingMachine";
 import "../styles/index.css"
+import {Navbar} from "./Navbar";
 
 export const MapComponent : React.FC = () => {
-    const position: [number, number] = [36.71580426431715, -4.455611828876954];
+    const position: [number, number] = [36.719213043469765, -4.455949667115419];
+    const position2: [number, number] = [36.72116082659559, -4.464346934397554];
+    const [isMapInit, setIsMapInit] = useState<boolean>(false);
+    const [map, setMap] = useState<any>();
+    const saveMap = (map: any) => {
+        setIsMapInit(true);
+        setMap(map);
+    };
     return (
       <>
+          <Navbar/>
           I'm trying the routing components from react.
-          <Map center={position} zoom={13} id="mapid">
+          <Map center={position} zoom={13} id="mapid" ref={saveMap}>
               <TileLayer
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                   attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
               />
               <Marker position={position}>
-                  <Popup>Comprar Papatas fritas</Popup>
+                  <Popup>Resi del Alejandro</Popup>
               </Marker>
+              <Marker position={position2}>
+                  <Popup>Resi del Parejo</Popup>
+              </Marker>
+              {isMapInit && <Routing map={map} fromCoordinates={position} toCoordinates={position2}/>}
+
           </Map>
       </>
   )
