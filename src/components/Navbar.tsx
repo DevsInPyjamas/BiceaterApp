ecf0acabfdbab7cf5ba22671b11400251a80b946
SomeWan72 <jugr98@gmail.com>
2019-11-26 21:59:11 +0100

    Edit user bottom added to the navbar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,15 +3,21 @@ import {WeatherComponent} from "./WeatherComponent";
 
 export const Navbar : React.FC = () => {
     return (
-        <nav className="navbar navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <span className="navbar-brand mb-0 h1">Biceater</span>
-            <div>
-                <WeatherComponent/>
+            <div className="collapse navbar-collapse">
+                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                </ul>
+                <div className="col-md-4">
+                    <WeatherComponent/>
+                </div>
+                <form className="form-inline">
+                    <input className="form-control mr-sm-2" type="search" placeholder="Usuario" aria-label="Search"/>
+                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+                </form>
+                <div className="divider-vertical"/>
+                <button type="button" className="btn btn-primary"  style= {{float: "right"}}>Editar Perfil</button>
             </div>
-            <form className="form-inline">
-                <input className="form-control mr-sm-2" type="search" placeholder="Usuario" aria-label="Search"/>
-                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-            </form>
         </nav>
     );
 };
