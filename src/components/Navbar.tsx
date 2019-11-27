56c4c96eb3eec4980f134b8314cba5aea1b3f978
pedrogilena@gmail.com <fairyTail22>
2019-11-28 00:29:26 +0100

    usersSeacrch

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,20 @@
-import React, {useState} from 'react';
-import {Redirect } from 'react-router';
+import React from 'react';
+import {Link} from "react-router-dom";
 
-export const Navbar : React.FC = () => {
+export const Navbar : React.FC<any> = (props) => {
 
-    const [user, setUser] = useState("");
+    const {user,onChangeUser} = props;
+
 
     const onSubmit = () => {
-        return ( <Redirect  to="/userSearch" />);
+        return ( <Link  to="/userSearch" />);
     };
 
     return (
         <nav className="navbar navbar-dark bg-dark">
             <span className="navbar-brand mb-0 h1">Biceater</span>
             <form className="form-inline">
-                <input className="form-control mr-sm-2" type="text" placeholder="Usuario" aria-label="Search" value={user}  onChange={e => setUser(e.target.value)}  />
+                <input className="form-control mr-sm-2" type="text" placeholder="Usuario" aria-label="Search" value={user}  onChange={onChangeUser}  />
                 <button className="btn btn-light" type="submit" onClick={onSubmit}> Buscar </button>
             </form>
         </nav>
--- src/components/userSearch.tsx
+++ src/components/userSearch.tsx
@@ -1,21 +1,40 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {Navbar} from "./Navbar";
 
-export const userSearch : React.FC = () => {
+export const userSearch: React.FC = () => {
+    const [user, setUser] = useState("");
+    const [allUsers, setAllUsers] = useState([]);
 
-    const filterByUser= () => {fetch('/views.users_by_username').then()};
-    const filterALlUser= () => {fetch('/views.all_users').then(r => string [])};
+    useEffect(() => {
+        fetch('/views.all_users')
+            .then(response => {
+                return response.json()
+            }).then(data => {
+                setAllUsers(data);
+            })
+    }, []);
 
+    const allUsersFiltered= allUsers.filter((actualUser:any)=>{
+        return actualUser.username.include(user);
+    }).map((actualUser:any)=>{
+        return(
+            <div className="card">
+            <h5 className="card-header">{actualUser.username}</h5>
+        <div className="card-body">
+            <button className="btn btn-light" type="submit"> Acceder Perfil</button>
+        </div>
+        </div>
+        );
+    });
 
     return (
         <div className="container">
-            <Navbar/>
+            <Navbar user={user} onChangeUser={(e: any) => {
+                setUser(e)
+            }}/>
             <h2>Usuarios</h2>
-            <div className="card">
-                <h5 className="card-header">Nombre usuario</h5>
-                <div className="card-body">
-                    <button className="btn btn-light" type="submit" > Acceder Perfil </button>
-                </div>
+            <div>
+                {allUsersFiltered}
             </div>
 
         </div>
