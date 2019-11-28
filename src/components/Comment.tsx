983ad8fd2f90d91596d8381faf71b02a6457f421
pedrogilena@gmail.com <fairyTail22>
2019-11-28 21:01:03 +0100

    Comments

---
+++ src/components/Comment.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+
+export const Comments: React.FC<any> = (props) => {
+
+    const {allComments} = props;
+
+    const allCommentsFiltered = allComments.filter((actualComment: any) => {
+        return actualComment;
+    }).map((actualComment: any) => {
+        return (
+            <div className="row" style={{marginBottom: "20px"}}>
+                <div className="col-2">
+
+                </div>
+                <div className="col-8">
+                    <div className="card">
+                        <h5 className="card-header">{actualComment.author}</h5>
+                        <div className="card-body">
+                            <p className="card-text"> {actualComment.text}</p>
+                            <div className="row">
+                                <footer className="col">
+                                    {actualComment.date}
+                                </footer>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="col-2">
+
+                </div>
+            </div>
+        );
+    });
+
+    return (allCommentsFiltered
+    );
+};
--- src/components/Parada.tsx
+++ src/components/Parada.tsx
@@ -1,89 +1,132 @@
-import React, {useState, useCallback} from 'react';
-import { datosAbiertos } from '../utils/RequestMaker';
-import {Navbar} from "./Navbar";
-import {MapComponent} from "./MapComponent";
+import React, { useState, useEffect } from 'react';
+import { MapComponent } from "./MapComponent";
+import { RouteComponentProps } from "react-router";
+import { createSequence } from "../utils/NumberUtilities";
+import {retrieveStation} from "../utils/RequestMaker";
+import {BikeHireDockingStation, ReducedBieHiringStation} from "../@types/Biceater";
 
+interface RouteParameters {
+    stationId: string;
+}
 
-export const Parada : React.FC = () => {
+type StationProps = RouteComponentProps<RouteParameters>
 
-    const range = (start: number, end: number) => {
-        return (new Array(end - start + 1)).fill(undefined).map((_, i) => i + start);
-    }
+export const Parada : React.FC<StationProps> = (props: StationProps) => {
 
-    const [parada, setParada] = useState({});
+    const stationId = parseInt(props.match.params.stationId);
 
-    const callback = useCallback(() => {
-        datosAbiertos().then((res: any) => {
-            console.log(res);
-            setParada(res);
-        })
-    }, []);
+    console.log(stationId);
+
+    const [station, setStation] = useState<BikeHireDockingStation>();
+    const [simpleStation, setSimpleStation] = useState<ReducedBieHiringStation[]>();
+    const [allComments, setAllComments] = useState([]);
+
+    useEffect(() => {
+        if(station === undefined) {
+            retrieveStation(stationId).then((result: BikeHireDockingStation) => {
+                setStation(result);
+                setSimpleStation([{
+                    [`${stationId}`]:
+                    [
+                        {
+                            type:  result.location.value.type,
+                            coordinates: [+result.location.value.coordinates[0], +result.location.value.coordinates[1]]
+                        },
+                        result.address.value
+                    ]
+                }]);
+            }).catch((err: any) => {
+                console.log('Fuck');
+            });
+        }
+    }, [station, stationId]);
+
+    console.log(simpleStation);
+
+    useEffect(() => {
+            fetch('/views.comment_by_stop')
+                .then(response => {
+                    return response.json()
+                }).then(data => {
+                    setAllComments(data);
+                })
+        }, []);
+
 
     return (
-    <div className="container">
-    <Navbar/>
-      <div className="row" style={{marginTop: "10px"}}>
-                <div className="col-6">
-                  <div className="card">
-                    <div className="card-body">
-                      Estacion XXX
+        <div className="container">
+            {station &&
+            <div>
+                <div className="row" style={{marginTop: "10px"}}>
+                    <div className="col-6">
+                        <div className="card">
+                            <div className="card-body">
+                                Estacion {station.id} en {station.address.value.streetAddress}
+                            </div>
+                        </div>
                     </div>
-                  </div>
+
+                    <div className="col-3">
+                        <div className="card">
+                            <div className="card-body">
+                                <select className="form-control" style={{marginRight: "10px"}}>
+                                    {createSequence(1, 10).map((el) => <option value={el} key={el}>{el}</option>)}
+                                </select>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="card">
+                            <div className="card-body">
+                                <button type="button" className="btn btn-primary" style={{float: "left"}}>Valora !
+                                </button>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="row" style={{marginTop: "20px", marginBottom: "20px"}}>
+
+                    <div className="col" style={{justifyContent: "center"}}>
+                        {simpleStation && <MapComponent allStations={simpleStation} zoom={17}/>}
+                    </div>
+
+                </div>
+                <div className="row" style={{marginBottom: "20px"}}>
+                    <div className="col-2">
+
+                    </div>
+                    <div className="col-8">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Comentario</h5>
+                                <form onClick={(event: any) => {
+                                    event.preventDefault()
+                                }}>
+                                    <div>
+                                            <textarea name="comments" id="comments">
+                                            Hey... say something!
+                                            </textarea>
+                                    </div>
+                                    <button type="button" className="btn btn-primary">Comenta!</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-2">
+
+                    </div>
                 </div>
 
-                <div className="col-3">
-                   <div className="card">
-                       <div className="card-body">
-                           <select className="form-control" style={{marginRight: "10px"}}>
-                             {range(1, 10).map((el) => <option value={el} key={el}>{el}</option>)}
-                            </select>
+                                    <Comments allComments={allComments}>
 
-                </div>
-                    </div>
-                     </div>
-                     <div className="col-3">
-                                        <div className="card">
-                                            <div className="card-body">
-                                                 <button type="button" className="btn btn-primary"  style= {{float: "left"}}>Valora  !</button>
 
-                                     </div>
-                                         </div>
-                                          </div>
-
-      </div>
-      <div className="row" style={{marginTop: "20px",marginBottom: "20px"}}>
-
-        <div className="col" style={{justifyContent: "center"}}> <MapComponent/> </div>
-
-      </div>
-      <div className="row" style={{marginBottom: "20px"}}>
-              <div className="col-2">
-
-              </div>
-              <div className="col-8">
-                <div className="card">
-                                   <div  className="card-body">
-                                     <h5 className="card-title">Comentario</h5>
-                                     <form onClick={(event: any) => {event.preventDefault()}}>
-                                                    <div>
-                                                    <textarea name="comments" id="comments" style={{width: "686px",resize: "both",height: "136px", marginBottom: "10px"}}>
-                                                    Hey... say something!
-                                                    </textarea>
-                                                    </div>
-                                                    <button type="button" className="btn btn-primary">Comenta!</button>
-                                                    </form>
-                                   </div>
-                                 </div>
-              </div>
-              <div className="col-2">
-
-              </div>
             </div>
-    </div>
+            }
+        </div>
 
     );
 };
-
-
-
 
